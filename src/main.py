9aad202998d90df88fafcc4269aed3430e823b0a
jan-menno <janmenno@yahoo.de>
2022-05-11 19:36:56 +0200

    Introduced saving, loading a collection, and removing or adding cards to it, as well as sorting

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 import ygo
 import ygo.monster as ym
 
-co = ym.get_xyz_monster()# ygo.load_collection("./11-May-2022_14-27-40.json")
-print(co.id)
-
-print(co.search_by_lvl_rank(9).sort(by='atk'))
-cm = co.search_by_lvl_rank(9).sort(by='atk')
-#cm.collection[0].download_image()
+co = ygo.get_all()
+cm = co.search_by_archetype('The Weather')
 cm.view()
